2324a4ec1a74e77275f013e0176d40e321abb555
temror <temror@gmail.com>
2021-09-10 14:26:24 +0300

    Final app commit

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require("path");
 
 const app = express()
 
 app.use(express.json({extended:true}))
+
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/places', require('./routes/place.routes'))
+
+if(process.env.NODE_ENV === 'production') {
+    app.use('/',express.static(path.join(__dirname,'client','build')))
+
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(path.resolve(__dirname,'client','build','index.html')))
+    })
+}
 
 const PORT = config.get('PORT') || 5000
 
